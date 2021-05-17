ba881f9221f9086e97ec6f7baf43722bc5fbbe5b
kazk <kazk.dev@gmail.com>
2021-05-17 16:41:27 -0700

    v0.2.0
    - Improve `expect_error` feedback

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="codewars_test",
-    version="0.1.0",
+    version="0.2.0",
     packages=["codewars_test"],
     license="MIT",
     description="Codewars test framework for Python",
     install_requires=[],
-    url="https://github.com/Codewars/python-test-framework",
+    url="https://github.com/codewars/python-test-framework",
 )
