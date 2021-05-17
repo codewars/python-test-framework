f987a76d4327bd1591ea43f69bad847a642c718b
Blind4Basics <32236948+Blind4Basics@users.noreply.github.com>
2021-05-17 23:04:45 +0200

    Update test_outputs.py

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -23,6 +23,9 @@ def test_against_expected(test_file, expected_file, env):
             expected = re.sub(
                 r"(?<=<COMPLETEDIN::>)\d+(?:\.\d+)?", r"\\d+(?:\\.\\d+)?", r.read()
             )
+            expected = re.sub(
+                r"Error\(([^)]*?)\)", r"Error\\(\1\\)", expected)
+                
             self.assertRegex(result.stdout.decode("utf-8"), expected)
 
     return test
