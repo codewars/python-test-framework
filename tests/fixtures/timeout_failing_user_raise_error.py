659475f66afa0197def7d989e2d7e002d0675a60
Blind4Basics <32236948+Blind4Basics@users.noreply.github.com>
2021-06-29 22:12:43 +0200

    corrections  on outputs

--- tests/fixtures/timeout_failing_user_raise_error.py
+++ tests/fixtures/timeout_failing_user_raise_error.py
@@ -3,7 +3,7 @@ import codewars_test as test
 
 @test.describe("group 1")
 def group_1():
-    @test.timeout(0.01)
+    @test.timeout(0.2)
     def test_1():
         raise KeyError()
         test.pass_()
--- tests/fixtures/timeout_failing_with_user_message.py
+++ tests/fixtures/timeout_failing_with_user_message.py
@@ -3,7 +3,7 @@ import codewars_test as test
 
 @test.describe("group 1")
 def group_1():
-    @test.timeout(0.2, "nope...")
+    @test.timeout(0.01, "nope...")
     def test_1():
         while True:
             pass
